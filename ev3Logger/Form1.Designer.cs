8be5fbe030a636710c59fa65a21877c614e18d73
kawakamiYuta <kawakamiYuta@>
2017-01-21 10:57:05 +0900

    init

---
+++ ev3Logger/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ev3Logger
+{
+    partial class Ev3Lover
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.LogFileWriterThread = new System.ComponentModel.BackgroundWorker();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(150, 12);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(102, 20);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(12, 12);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(121, 20);
+            this.cmbPortName.TabIndex = 1;
+            // 
+            // LogFileWriterThread
+            // 
+            this.LogFileWriterThread.DoWork += new System.ComponentModel.DoWorkEventHandler(this.LogFileWriterThread_DoWork);
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Location = new System.Drawing.Point(150, 59);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(102, 24);
+            this.SelectFileButton.TabIndex = 2;
+            this.SelectFileButton.Text = "SelectFile";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FileNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FileNameLabel.Location = new System.Drawing.Point(12, 59);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(121, 24);
+            this.FileNameLabel.TabIndex = 5;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(97, 216);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(95, 33);
+            this.Start.TabIndex = 6;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Ev3Lover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.FileNameLabel);
+            this.Controls.Add(this.SelectFileButton);
+            this.Controls.Add(this.cmbPortName);
+            this.Controls.Add(this.ConnectButton);
+            this.Name = "Ev3Lover";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort SerialPort;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.ComponentModel.BackgroundWorker LogFileWriterThread;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.Label FileNameLabel;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ ev3Logger/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ev3Logger
+{
+    public partial class Ev3Lover : Form
+    {
+        static bool _cont;
+        public Ev3Lover()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //! 利用可能なシリアルポート名の配列を取得する.
+            string[] PortList = System.IO.Ports.SerialPort.GetPortNames();
+
+            cmbPortName.Items.Clear();
+
+            //! シリアルポート名をコンボボックスにセットする.
+            foreach (string PortName in PortList)
+            {
+                cmbPortName.Items.Add(PortName);
+            }
+            if (cmbPortName.Items.Count > 0)
+            {
+                cmbPortName.SelectedIndex = 0;
+            }
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (SerialPort.IsOpen == true)
+            {
+                //! シリアルポートをクローズする.
+                SerialPort.Close();
+
+                //! ボタンの表示を[切断]から[接続]に変える.
+                ConnectButton.Text = "Connect";
+
+                _cont = false;
+            }
+            else
+            {
+                //! オープンするシリアルポートをコンボボックスから取り出す.
+                SerialPort.PortName = cmbPortName.SelectedItem.ToString();
+
+                //! ボーレートをコンボボックスから取り出す.
+                SerialPort.BaudRate = 9600;
+
+                //! データビットをセットする. (データビット = 8ビット)
+                SerialPort.DataBits = 8;
+
+                //! パリティビットをセットする. (パリティビット = なし)
+                SerialPort.Parity = Parity.None;
+
+                //! ストップビットをセットする. (ストップビット = 1ビット)
+                SerialPort.StopBits = StopBits.One;
+
+                //! フロー制御をコンボボックスから取り出す.
+                SerialPort.Handshake = Handshake.None;
+
+                //! 文字コードをセットする.
+                SerialPort.Encoding = Encoding.ASCII;
+
+                SerialPort.NewLine = "\n";
+
+                try
+                {
+                    //! シリアルポートをオープンする.
+                    SerialPort.Open();
+
+                    //! ボタンの表示を[接続]から[切断]に変える.
+                    ConnectButton.Text = "DisConnect";
+
+                    //! ログスレッドを開始する。
+                    _cont = true;
+                    LogFileWriterThread.RunWorkerAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void LogFileWriterThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string path = Environment.CurrentDirectory + @"\" + System.DateTime.Now.Year.ToString()+
+                System.DateTime.Now.Month.ToString() + System.DateTime.Now.Day.ToString() + 
+                System.DateTime.Now.Hour.ToString() + System.DateTime.Now.Minute.ToString() +
+                System.DateTime.Now.Second.ToString() + ".csv";
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.AutoFlush = true;
+                sw.NewLine = "\n";
+                while (_cont)
+                {
+                    try
+                    {
+                        char c = (char)SerialPort.ReadChar();
+                        if(c == '\r')
+                        {
+                            SerialPort.WriteLine(c.ToString());
+                        }
+                        else
+                        {
+                            SerialPort.Write(c.ToString());
+                        }
+                        sw.WriteLine(c.ToString());
+                    }
+                    catch (Exception) { break; }
+                }
+            }
+        }
+
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = "default.html";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //2番目の「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                FileNameLabel.Text = ofd.FileName;
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(FileNameLabel.Text));
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine() + '\r';
+                    SerialPort.WriteLine(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
